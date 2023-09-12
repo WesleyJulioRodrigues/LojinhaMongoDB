8e78579a17698e1ea681086929340561716068d8
Wesleyy <113564460+WesleyJulioRodrigues@users.noreply.github.com>
2023-09-12 20:15:04 -0300

    Lojinhadb

---
+++ LojinhaServer/Extentions/ServiceExtensions.cs
@@ -0,0 +1,34 @@
+using LojinhaServer.Repositories;
+using LojinhaServer.Models;
+using MongoDB.Driver;
+namespace LojinhaServer.Extensions;
+public static class ServiceExtensions
+{
+public static void ConfigureCors(this IServiceCollection services)
+{
+services.AddCors(options =>
+{
+options.AddPolicy("CorsPolicy",
+builder => builder.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader());
+});
+}
+public static void ConfigureMongoDBSettings(this IServiceCollection services,
+IConfiguration config)
+{
+services.Configure<MongoDBSettings>(
+config.GetSection("MongoDBSettings")
+);
+services.AddSingleton<IMongoDatabase>(options => {
+var settings =
+config.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+var client = new MongoClient(settings.ConnectionString);
+return client.GetDatabase(settings.DatabaseName);
+});
+}
+public static void ConfigureProductRepository(this IServiceCollection services)
+{
+services.AddSingleton<IProductRepository, ProductRepository>();
+}
+}
